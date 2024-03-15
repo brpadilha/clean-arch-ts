3f45512e2760bb0fe5aa80f65a1631349e5432de
Bruno Padilha <brpadilha.dev@gmail.com>
2024-03-15 13:25:46 -0300

    configura jest

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+  // diretorio onde queremos que seja feita a cobertura de testes
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: "coverage",
+  coverageProvider: "babel",
+  // para quando encontrar o path com @, ele vai substituir pelo rootdir/src
+  moduleNameMapper: {
+    '@/tests/(.+)':'<rootDir>/tests/$1',
+    '@/(.+)':'<rootDir>/src/$1'
+  },
+  roots: [
+    "<rootDir>/src",
+    "<rootDir>/tests"
+  ],
+  // para o jest interpretar typescript
+  transform: {
+    '\\.ts$': 'ts-jest'
+  }
+
+};
+
+module.exports = config;
