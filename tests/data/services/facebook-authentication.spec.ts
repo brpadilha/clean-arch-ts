e5fd6ed2022881fa958b0d5bfee448cfa3a6611a
Bruno Padilha <brpadilha.dev@gmail.com>
2024-03-18 14:17:22 -0300

    fix package

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -31,7 +31,7 @@ describe('FacebookAuthenticationService', () => {
     const loadFacebookUserApi = new LoadFacebookUserApiSpy()
     const sut = new FacebookAuthenticationService(loadFacebookUserApi)
 
-    await sut.perform({ token: 'any_token' })
+    await sut.perform({ token: 'any_toke2n' })
 
     expect(loadFacebookUserApi.token).toBe('any_token')
   })
