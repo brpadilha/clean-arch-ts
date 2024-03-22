92e67d21cd0d97d89fc4c100fde322064d799107
Bruno Padilha <brpadilha.dev@gmail.com>
2024-03-22 13:14:22 -0300

    ensure Service calls LoadUserAccountRepo when loadFacebookUserApi return data

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,15 +1,27 @@
 import { LoadFacebookUserApi } from '@/data/contracts/apis'
+import { LoadUserAccountRepository } from '@/data/contracts/repos'
 import { FacebookAuthenticationService } from '@/data/services'
 import { AuthenticationError } from '@/domain/errors'
 import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('FacebookAuthenticationService', () => {
   let loadFacebookUserApi: MockProxy<LoadFacebookUserApi>
+  let loadUserAccountRepo: MockProxy<LoadUserAccountRepository>
   let sut: FacebookAuthenticationService
   const token = 'any_token'
   beforeEach(() => {
-    loadFacebookUserApi = mock<LoadFacebookUserApi>()
-    sut = new FacebookAuthenticationService(loadFacebookUserApi)
+    loadUserAccountRepo = mock()
+    loadFacebookUserApi = mock()
+    // remover o mockResolvedValueOnce e passar para mockResolvedValue
+    // para que quando formos mockar novamente para undefined com o mockResolvedValueOnce ele não empilhe ou sobrescreva
+
+    loadFacebookUserApi.loadUser.mockResolvedValue({
+      facebookId: 'any_fb_id',
+      name: 'any_fb_name',
+      email: 'any_fb_email'
+    })
+
+    sut = new FacebookAuthenticationService(loadFacebookUserApi, loadUserAccountRepo)
   })
 
   it('should call LoadFacebookUserApi api with correct params', async () => {
@@ -25,5 +37,14 @@ describe('FacebookAuthenticationService', () => {
     const authResult = await sut.perform({ token })
 
     expect(authResult).toEqual(new AuthenticationError())
+  })
+
+  it('should call LoadUserAccountRepo when LoadFacebookUserApi returns data', async () => {
+    await sut.perform({ token })
+
+    expect(loadUserAccountRepo.load).toHaveBeenCalledWith({
+      email: 'any_fb_email'
+    })
+    expect(loadUserAccountRepo.load).toHaveBeenCalledTimes(1)
   })
 })
