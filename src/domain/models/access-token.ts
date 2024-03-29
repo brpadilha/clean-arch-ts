a3f6db5ccbabd39a48226eb2547a76fd4d33b338
Bruno Padilha <brpadilha.dev@gmail.com>
2024-03-29 19:23:01 -0300

    ensure accessToken expires in 30 minutes

--- src/domain/models/access-token.ts
+++ src/domain/models/access-token.ts
@@ -1,5 +1,9 @@
 export class AccessToken {
-  constructor(private readonly value: string) {
+  constructor(private readonly value: string) { }
 
+  static get expirationInMs(): number {
+    // 30 minutes * 60 seconds * 1000 milisegundos
+    // 1800000 ms
+    return 30 * 60 * 1000
   }
 }
