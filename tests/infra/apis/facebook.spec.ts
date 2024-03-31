bf8cc76fceb889a3e8c554172580737129f5c561
Bruno Padilha <brpadilha.dev@gmail.com>
2024-03-31 00:08:53 -0300

    ensure FacebookApi get app token with correct params

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -1,15 +1,22 @@
 import { FacebookAuthentication } from '@/domain/features'
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
 
 class FacebookApi {
   private readonly baseUrl = 'https://graph.facebook.com'
   constructor(
-    private readonly httpClient: HttpGetClient
+    private readonly httpClient: HttpGetClient,
+    private readonly clientId: string,
+    private readonly clientSecret: string
   ) { }
 
   async loadUser(params: FacebookAuthentication.Params): Promise<void> {
     await this.httpClient.get({
-      url: `${this.baseUrl}/outh/access_token`
+      url: `${this.baseUrl}/outh/access_token`,
+      params: {
+        client_id: this.clientId,
+        client_secret: this.clientSecret,
+        grant_type: 'client_credential'
+      }
     })
   }
 }
@@ -21,17 +28,31 @@ export interface HttpGetClient {
 namespace HttpGetClient {
   export type Params = {
     url: string
+    params: object
   }
 }
 
 describe('FacebookApi', () => {
+  let httpClient: MockProxy<HttpGetClient>
+  let token: string
+  beforeAll(() => {
+    httpClient = mock()
+    token = 'any_client_token'
+  })
   it('should get app token', async () => {
-    const httpClient = mock<HttpGetClient>()
-    const sut = new FacebookApi(httpClient)
+    const clientId = 'any_client_id'
+    const clientSecret = 'any_client_secret'
 
-    await sut.loadUser({ token: 'any_client_token' })
+    const sut = new FacebookApi(httpClient, clientId, clientSecret)
+
+    await sut.loadUser({ token })
     expect(httpClient.get).toHaveBeenCalledWith({
-      url: 'https://graph.facebook.com/outh/access_token'
+      url: 'https://graph.facebook.com/outh/access_token',
+      params: {
+        client_id: clientId,
+        client_secret: clientSecret,
+        grant_type: 'client_credential'
+      }
     })
   })
 })
