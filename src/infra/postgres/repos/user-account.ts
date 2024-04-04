9b9e99c2ff2d24eb9bcaa53ad00a45f27b5c7f75
Bruno Padilha <brpadilha.dev@gmail.com>
2024-04-03 21:12:31 -0300

    ensure PgUserAccountRepository save user

--- src/infra/postgres/repos/user-account.ts
+++ src/infra/postgres/repos/user-account.ts
@@ -1,16 +1,43 @@
-import { LoadUserAccountRepository } from '@/data/contracts/repos'
+import { LoadUserAccountRepository, SaveFacebookAccountRepository } from '@/data/contracts/repos'
 import { getRepository } from 'typeorm'
 import { PgUser } from '../entities'
 
-export class PgUserAccountRepository implements LoadUserAccountRepository {
-  async load(params: LoadUserAccountRepository.Params): Promise<LoadUserAccountRepository.Result> {
-    const pgUserRepo = getRepository(PgUser)
-    const pgUser = await pgUserRepo.findOne({ email: params.email })
+type LoadParams = LoadUserAccountRepository.Params
+type LoadResult = LoadUserAccountRepository.Result
+type SaveParams = SaveFacebookAccountRepository.Params
+type SaveResult = SaveFacebookAccountRepository.Result
+export class PgUserAccountRepository implements LoadUserAccountRepository, SaveFacebookAccountRepository {
+  private readonly pgUserRepo = getRepository(PgUser)
+  async load(params: LoadParams): Promise<LoadResult> {
+    const pgUser = await this.pgUserRepo.findOne({ email: params.email })
     if (pgUser !== undefined) {
       return {
         id: pgUser.id.toString(),
         name: pgUser.name ?? undefined
       }
+    }
+  }
+
+  async saveWithFacebook(params: SaveParams): Promise<SaveResult> {
+    let id: string
+    if (params.id === undefined) {
+      const pgUser = await this.pgUserRepo.save({
+        email: params.email,
+        name: params.name,
+        facebookId: params.facebookId
+      })
+      id = pgUser.id.toString()
+    } else {
+      id = params.id
+      await this.pgUserRepo.update({
+        id: 1
+      }, {
+        email: params.email,
+        facebookId: params.facebookId
+      })
+    }
+    return {
+      id
     }
   }
 }
--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -6,25 +6,25 @@ import { PgUser } from '@/infra/postgres/entities'
 import { makeFakeDb } from '@/tests/infra/postgres/mocks/connection'
 
 describe('PgUserAccountRepository', () => {
+  let sut: PgUserAccountRepository
+  let pgUserRepo: Repository<PgUser>
+  let backup: IBackup
+  beforeAll(async () => {
+    const db = await makeFakeDb([PgUser])
+    // criando um ponto de backup para depois resetarmos o banco antes de cada teste
+    backup = db.backup()
+    pgUserRepo = getRepository(PgUser)
+  })
+
+  afterAll(async () => {
+    await getConnection().close()
+  })
+
+  beforeEach(() => {
+    backup.restore()
+    sut = new PgUserAccountRepository()
+  })
   describe('load', () => {
-    let sut: PgUserAccountRepository
-    let pgUserRepo: Repository<PgUser>
-    let backup: IBackup
-    beforeAll(async () => {
-      const db = await makeFakeDb([PgUser])
-      // criando um ponto de backup para depois resetarmos o banco antes de cada teste
-      backup = db.backup()
-      pgUserRepo = getRepository(PgUser)
-    })
-
-    afterAll(async () => {
-      await getConnection().close()
-    })
-
-    beforeEach(() => {
-      backup.restore()
-      sut = new PgUserAccountRepository()
-    })
     it('should return an account if email exists', async () => {
       await pgUserRepo.save({ email: 'existing_email' })
 
@@ -34,10 +34,48 @@ describe('PgUserAccountRepository', () => {
     })
 
     it('should return undefined if email does not exists', async () => {
-      const sut = new PgUserAccountRepository()
       const account = await sut.load({ email: 'existing_email' })
 
       expect(account).toEqual(undefined)
+    })
+  })
+
+  describe('saveWithFacebook', () => {
+    it('should create an account if id is undefined', async () => {
+      const { id } = await sut.saveWithFacebook({
+        facebookId: 'any_facebook_id',
+        name: 'any_name',
+        email: 'any_email'
+      })
+
+      const pgUser = await pgUserRepo.findOne({ email: 'any_email' })
+
+      expect(pgUser?.id).toBe(1)
+      expect(id).toBe('1')
+    })
+    it('should update account if id is defined', async () => {
+      await sut.saveWithFacebook({
+        facebookId: 'any_facebook_id',
+        name: 'any_name',
+        email: 'any_email'
+      })
+
+      const { id } = await sut.saveWithFacebook({
+        id: '1',
+        facebookId: 'new_facebook_id',
+        name: 'new_name',
+        email: 'new_email'
+      })
+
+      const pgUser = await pgUserRepo.findOne({ id: 1 })
+
+      expect(pgUser).toEqual({
+        id: 1,
+        name: 'any_name',
+        email: 'new_email',
+        facebookId: 'new_facebook_id'
+      })
+      expect(id).toBe('1')
     })
   })
 })
