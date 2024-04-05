5b4b37df8bd5f4a106850e68e71752a399f8a3f8
Bruno Padilha <brpadilha.dev@gmail.com>
2024-04-05 09:07:59 -0300

    ensure FacebookLoginController calls FacebookAuth with correct params

--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -1,12 +1,20 @@
+import { FacebookAuthentication } from '@/domain/features'
+import { mock, MockProxy } from 'jest-mock-extended'
+
 describe('FacebookLoginController', () => {
   let sut: FacebookLoginController
+  let facebookAuthentication: MockProxy<FacebookAuthentication>
 
   beforeAll(() => {
-    sut = new FacebookLoginController()
+    facebookAuthentication = mock()
+    sut = new FacebookLoginController(facebookAuthentication)
+  })
+
+  beforeEach(() => {
   })
 
   it('should return 400 if token is empty', async () => {
-    const httpResponse = await sut.handler({ token: '' })
+    const httpResponse = await sut.handle({ token: '' })
 
     expect(httpResponse).toEqual({
       statusCode: 400,
@@ -15,7 +23,7 @@ describe('FacebookLoginController', () => {
   })
 
   it('should return 400 if token is null', async () => {
-    const httpResponse = await sut.handler({ token: null })
+    const httpResponse = await sut.handle({ token: null })
 
     expect(httpResponse).toEqual({
       statusCode: 400,
@@ -24,17 +32,33 @@ describe('FacebookLoginController', () => {
   })
 
   it('should return 400 if token is undefined', async () => {
-    const httpResponse = await sut.handler({ token: undefined })
+    const httpResponse = await sut.handle({ token: undefined })
 
     expect(httpResponse).toEqual({
       statusCode: 400,
       data: new Error('The field token is required')
     })
   })
+
+  it('should call facebook authentication with correct params', async () => {
+    await sut.handle({ token: 'any_token' })
+
+    expect(facebookAuthentication.perform).toHaveBeenCalledWith({
+      token: 'any_token'
+    })
+    expect(facebookAuthentication.perform).toHaveBeenCalledTimes(1)
+  })
 })
 
 class FacebookLoginController {
-  async handler(params: any): Promise<HttpResponse> {
+  constructor(private readonly facebookAuthentication: FacebookAuthentication) { }
+  async handle(httpRequest: any): Promise<HttpResponse> {
+    await this.facebookAuthentication.perform(
+      {
+        token: httpRequest.token
+      }
+    )
+
     return {
       statusCode: 400,
       data: new Error('The field token is required')
