c75a37628fc957ec935003d49470ebc9d3be3342
Bruno Padilha <brpadilha.dev@gmail.com>
2024-04-09 12:23:09 -0300

    move RequiredStringValidator to its own file

--- tests/application/validation/required-string.spec.ts
+++ tests/application/validation/required-string.spec.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { RequiredFieldError } from '@/application/errors'
+import { RequiredStringValidator } from '@/application/validation'
 
 describe('RequiredStringValidator', () => {
   it('should return an RequiredFieldError if value is empty', () => {
@@ -25,14 +26,12 @@ describe('RequiredStringValidator', () => {
 
     expect(error).toEqual(new RequiredFieldError('any_field'))
   })
+
+  it('should return undefined if value is not empty', () => {
+    const sut = new RequiredStringValidator('any_value', 'any_field')
+
+    const error = sut.validate()
+
+    expect(error).toEqual(undefined)
+  })
 })
-
-class RequiredStringValidator {
-  constructor(
-    private readonly value: string,
-    private readonly fieldName: string) { }
-
-  validate(): Error | undefined {
-    return new RequiredFieldError('any_field')
-  }
-}
